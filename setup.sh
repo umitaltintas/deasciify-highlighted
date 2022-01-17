7dd6c5231b06e1d8749898739ecacec53176d5b2
umit <umitaltintas1@gmail.com>
2022-01-17 23:03:49 +0300

    update setup

--- setup.sh
+++ setup.sh
@@ -4,13 +4,13 @@ import sys
 import subprocess
 import pkg_resources
 
-required = {'Turkish-Deasciifier', 'pyperclip','pynput'}
+required = {'turkish-deasciifier', 'pyperclip','pynput'}
 installed = {pkg.key for pkg in pkg_resources.working_set}
 missing = required - installed
 
 if missing:
     python = sys.executable
-    subprocess.check_call([python, '-m', 'pip', 'install', *missing], stdout=subprocess.DEVNULL)
+    subprocess.check_call([python, '-m', 'pip', 'install', *missing])
 
 
 
